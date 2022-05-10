adbaefc4683fd343a180e9831fe0c7a71c47b2ee
hakase-monoshiri <ebenaeleazer@outlook.com>
2022-05-10 18:53:25 -0400

    move email presence validation to inside custom validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 
     has_many :pantries
 
-    validates :email, presence: :true, :uniqueness => true
+    validates :email, :uniqueness => true
     validates_with EmailValidator
 end
