06ade3b85f14a43a398266941e0a2d312ed4b350
hakase-monoshiri <ebenaeleazer@outlook.com>
2022-05-12 20:20:24 -0400

    write test to make sure pantry has ingredients

--- test/models/pantry_test.rb
+++ test/models/pantry_test.rb
@@ -4,4 +4,10 @@ class PantryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'have a list of ingredients' do
+    assert_not_nil Pantry.first.ingredients, 'Should have included ingredients'
+  end
+
+  
 end
