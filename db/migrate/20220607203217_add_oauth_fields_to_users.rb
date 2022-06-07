0a3f4e418532a90a9eb98d6217c381b6f0911f8a
Eben Eleazer <ebenaeleazer@outlook.com>
2022-06-07 16:33:39 -0400

    add oauth fields to user model

---
+++ db/migrate/20220607203217_add_oauth_fields_to_users.rb
@@ -0,0 +1,7 @@
+class AddOauthFieldsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_13_002936) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_203217) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_13_002936) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "image"
   end
 
   add_foreign_key "pantries", "users"
