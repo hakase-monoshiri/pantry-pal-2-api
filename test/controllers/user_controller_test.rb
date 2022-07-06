c54106a9276a997f84ee5ad98b8800c1d2b80576
Eben Eleazer <ebenaeleazer@outlook.com>
2022-07-05 20:32:04 -0400

    create integration tests for memeber data access

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class UserControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers 
   # test "the truth" do
   #   assert true
   # end
+  test "Is able to sign in" do
+    sign_in users(:one)
+
+    get users_member_data_path
+    
+    assert_response :success
+  end
+
+  test "does not allow access to non signed in users" do
+    get users_member_data_path
+
+    assert_response :redirect
+
+    sign_in users(:one)
+
+    get users_member_data_path
+
+    assert_response :success
+  end
+
+
 end
