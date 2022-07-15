6e5b54d617b05cdec036c0784a73a5c0c9d0c85c
Eben Eleazer <ebenaeleazer@outlook.com>
2022-07-15 19:18:03 -0400

    Add search method to recipie
    Recipies now have a search method that queries and return
    response from Edamam api

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,13 @@
+require 'httparty'
+
 class Recipe < ApplicationRecord
+
+
+    def self.search(query)
+
+        results = HTTParty.get("https://api.edamam.com/api/recipes/v2?type=public&q=#{query}&app_id=#{Rails.application.credentials.edamam[:app_id]}&app_key=#{Rails.application.credentials.edamam[:app_key]}&random=true")
+
+        results
+    end
+
 end
