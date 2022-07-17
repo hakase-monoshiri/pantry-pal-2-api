23d0a20ab242ca9d0f85f7ee0828acdf7a13ab2e
Eben Eleazer <ebenaeleazer@outlook.com>
2022-07-17 14:46:50 -0400

    Add documentation to Recipe.search function
    This recipe search will eventually be used with a pantry to find
    a small number (1 -3?) recipe reccomendations based on ingredients on hand.
    
    As written it simply searches based on inputed string query.
    
    These types of searches will eventually be handled by the client application

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,13 @@ class Recipe < ApplicationRecord
     has_many :ingredients
 
 
+
+    # Will probably handle large searches with multiple results by calling the EDAMAM API client side
+    # This server side search will be mostly for searching based on a users pantry
+
+
+    # for now it'll stay like this
+
     def self.search(query)
 
         response = HTTParty.get("https://api.edamam.com/api/recipes/v2?type=public&q=#{query}&app_id=#{Rails.application.credentials.edamam[:app_id]}&app_key=#{Rails.application.credentials.edamam[:app_key]}&random=true").body
