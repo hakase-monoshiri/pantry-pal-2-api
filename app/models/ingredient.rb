10c05fa986ae18abef323824c4c3e0d3b894b6d4
Eben Eleazer <ebenaeleazer@outlook.com>
2022-07-17 14:33:38 -0400

    Create association between ingredient and recipe model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,6 @@
 class Ingredient < ApplicationRecord
 
     belongs_to :pantry
+    belongs_to :recipe
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,12 +2,17 @@ require 'httparty'
 
 class Recipe < ApplicationRecord
 
+    has_many :ingredients
+
 
     def self.search(query)
 
-        results = HTTParty.get("https://api.edamam.com/api/recipes/v2?type=public&q=#{query}&app_id=#{Rails.application.credentials.edamam[:app_id]}&app_key=#{Rails.application.credentials.edamam[:app_key]}&random=true").hits
+        response = HTTParty.get("https://api.edamam.com/api/recipes/v2?type=public&q=#{query}&app_id=#{Rails.application.credentials.edamam[:app_id]}&app_key=#{Rails.application.credentials.edamam[:app_key]}&random=true").body
 
-        results[0..9]
+        result = JSON.parse response, symbolize_names: true
+
+    
+        puts result[:hits].first
     end
 
 end
